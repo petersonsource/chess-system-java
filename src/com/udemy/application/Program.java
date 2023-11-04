a430da151b07489cc94632f9ce30eff0818e694c
Peterson dos Santos <peterson.opensource@hotmail.com>
2023-11-03 22:36:45 -0300

    Fist Class: Position

--- src/com/udemy/application/Program.java
+++ src/com/udemy/application/Program.java
@@ -1,8 +1,13 @@
 package com.udemy.application;
 
+import com.udemy.boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Position  position = new Position(1, 2);
+		System.out.println(position);
 
 	}
 
